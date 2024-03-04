3bc01f13d42e23505cec17b6c3c46b1be98a31d5
Martin Bhuong <martin@genius.ke>
2024-03-04 15:27:24 +0000

    --

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/binwiederhier/ntfy:${NTFY_TAG" \
+    --label="org.nethserver.images=docker.io/binwiederhier/ntfy:${NTFY_TAG}" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
